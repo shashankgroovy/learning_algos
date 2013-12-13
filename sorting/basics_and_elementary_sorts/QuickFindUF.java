345903451776d9f447f157a3eb5bb50b32794280
shashankgroovy <shashankgrovy@gmail.com>
2013-12-14 00:18:20 +0530

    add quickfind

---
+++ sorting/basics_and_elementary_sorts/QuickFindUF.java
@@ -0,0 +1,24 @@
+//Filename: QuickFindUF.java
+
+public class QuickFindUF
+{
+    private int[] id;
+
+    public QuickFindUF(int n)
+    {
+        id = new int[n];
+        for (int i = 0; i< n; i++)
+            id[i] = i;
+    }
+
+    public boolean coneected(int p, int q)
+    { return id[p] == id[q]; }
+
+    public void union(int p, int q)
+    {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pid) id[i] = qid;
+    }
+}
