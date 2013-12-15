c5da4bb49bb61044f85d3ca20cc8e46dee4cf43d
shashankgroovy <shashankgrovy@gmail.com>
2013-12-16 02:48:12 +0530

    add bubblesort

---
+++ sorting/basics_and_elementary_sorts/bubblesort.py
@@ -0,0 +1,12 @@
+def bubblesort(arr):
+    """ Implementation of bubble sort. It is an O(n^2) sorting algorithm.
+    """
+    for i in range(len(arr)):
+        for j in range(i ,len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+if __name__=='__main__':
+    print selectionsort([8, 0, 2, 13, 5, 1, 21, 3, 1])
+
